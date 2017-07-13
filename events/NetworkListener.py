78a8fc2f729e23625fb84760c4da9ed7407b1e8c
andrey <andrey@gmail.com>
2017-07-13 16:28:43 +0300

    fires

--- events/NetworkListener.py
+++ events/NetworkListener.py
@@ -12,35 +12,37 @@ class NetworkListener(ConnectionListener):
     def __init__(self, host, port):
         self.Connect((host, port))
 
-    def Network(self, data):
+    def Network(self, update):
 
-        print time(), data
+        print time(), update
 
-        if data.get('action') == Global.NetworkActions.INIT:
-            if data.get('walls'):
-                self.events.set_walls(data.get('walls'))
+        if update.get('action') == Global.NetworkActions.INIT:
+            if update.get('walls'):
+                self.events.set_walls(update.get('walls'))
 
-            if data.get('id'):
-                Global.CurrentPlayerId = data.get('id')
+            if update.get('id'):
+                Global.CurrentPlayerId = update.get('id')
 
-        if data.get('action') == Global.NetworkActions.UPDATE:
-            self.events.gameObject.update(data)
+        for data in update.get('data', []):
 
-        if data.get('action') == Global.NetworkActions.UPDATE_BATCH:
-            for player_data in data.get('objects'):
-                self.events.gameObject.update(player_data)
+            if data.get('action') == Global.NetworkActions.UPDATE:
+                self.events.gameObject.update(data)
 
-        if data.get('action') == Global.NetworkActions.TEST:
-            self.events.gameObject.test(data)
+            if data.get('action') == Global.NetworkActions.UPDATE_BATCH:
+                for player_data in data.get('objects'):
+                    self.events.gameObject.update(player_data)
 
-        if data.get('action') == Global.NetworkActions.TANK_FIRE:
-            self.events.gameObject.fire(data)
+            if data.get('action') == Global.NetworkActions.TEST:
+                self.events.gameObject.test(data)
 
-        if data.get('action') == Global.NetworkActions.DAMAGE:
-            self.events.gameObject.damage(data)
+            if data.get('action') == Global.NetworkActions.TANK_FIRE:
+                self.events.gameObject.fire(data)
 
-        if data.get('action') == Global.NetworkActions.DESTROY:
-            self.events.gameObject.destroy(data)
+            if data.get('action') == Global.NetworkActions.DAMAGE:
+                self.events.gameObject.damage(data)
+
+            if data.get('action') == Global.NetworkActions.DESTROY:
+                self.events.gameObject.destroy(data)
 
     #https://www.youtube.com/watch?v=AdG_ITCFHDI EXPLODIONS !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
 
